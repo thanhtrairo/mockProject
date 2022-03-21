c3b30cc80f9b3de3a848b88b2e79cdd3ea920d27
thanhtrairo <tienthanhcntn789@gmail.com>
2022-03-21 15:28:21 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ProductDetail from './features/products/Components/ProductDetail';
 function App() {
   return (
     <div className="App">
-      <ProductDetail />
+      <Products />
     </div>
   );
 }
